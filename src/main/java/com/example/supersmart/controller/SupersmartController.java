7a4ef460d18d5ccc7e631dbb46b7902e9005ea6f
gi2019 <guyg7@mail.afeka.ac.il>
2023-04-14 15:07:47 +0300

    New /api dummy endpoint

--- src/main/java/com/example/supersmart/controller/SupersmartController.java
+++ src/main/java/com/example/supersmart/controller/SupersmartController.java
@@ -6,10 +6,7 @@ import com.example.supersmart.entity.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -17,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SupersmartController {
     @Autowired
     private Validator validator;
+
+    @GetMapping("")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("API is up");
+    }
 
     @PostMapping("/validate")
     public ResponseEntity<CustomResponse> validate(@RequestBody Transaction transaction) {
